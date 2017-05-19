0aca5a305cde88a421277e5d3e29044f724c0111
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2017-05-18 19:01:50 -0600

    Agregando Waitress...

---
+++ MenuComposite/src/app/Waitress.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.util.Iterator;
+
+import menuComposite.MenuComponent;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
